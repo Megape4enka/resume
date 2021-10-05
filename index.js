e5d6fc192ef0e7c65aa7d94bdf0a8c4671f03bc6
HP <mega.pe4enka@gmail.com>
2021-10-05 23:52:30 +0300

    Added images

---
+++ index.js
@@ -0,0 +1,34 @@
+let toggle = document.querySelector('.toggle')
+let topbar = document.querySelector('.topbar')
+let navigation = document.querySelector('.navigation')
+let main = document.querySelector('.main')
+let checkbox = document.querySelector('.checkbox')
+let body = document.querySelector('body')
+
+toggle.onclick = function (){
+    toggle.classList.toggle('active')
+    topbar.classList.toggle('active')
+    navigation.classList.toggle('active')
+    main.classList.toggle('active')
+}
+
+// Theme switch toggle
+
+checkbox.onclick = function () {
+    body.classList.toggle('dark')
+}
+
+// Now, we fix menu active class issue for mobile devices
+function toggleMenu() {
+    let navigation = document.querySelector('.navigation')
+    let main = document.querySelector('.main')
+    navigation.classList.remove('active')
+    main.classList.remove('active')
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 1000,
+        behavior: 'smooth'
+    });
+}
